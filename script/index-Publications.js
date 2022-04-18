0de0ca29359051b106388e3d4fd31615e0cb1ff0
Gyaaaaaaan <comteatedes@gmail.com>
2022-04-18 03:19:06 -0300

    Conserto de bug

--- script/index-Publications.js
+++ script/index-Publications.js
@@ -4,33 +4,29 @@ const Artigoesp = document.getElementById("Artigoesp");
 const Artigoen = document.getElementById("Artigoen");
 
 function ShowEspanholTranslation() {
-	Artigoptbr.style.display = "none";
-	Artigoen.style.display = "none";
-	Artigoesp.style.display = "block";
+    Artigoptbr.style.display = "none";
+    Artigoen.style.display = "none";
+    Artigoesp.style.display = "block";
 
 }
 function ShowBrasilTranslation() {
-	Artigoen.style.display = "none";
-	Artigoesp.style.display = "none";
-	Artigoptbr.style.display = "block";
+    Artigoen.style.display = "none";
+    Artigoesp.style.display = "none";
+    Artigoptbr.style.display = "block";
 }
 function ShowInglesTranslation() {
-	Artigoptbr.style.display = "none";
-	Artigoesp.style.display = "none";
-	Artigoen.style.display = "block";
+    Artigoptbr.style.display = "none";
+    Artigoesp.style.display = "none";
+    Artigoen.style.display = "block";
 }
 
-darktoggle2.onclick = function() {
+darktoggle2.onclick = function () {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
         darktoggle.innerHTML = "Light mode"
 
-        // icon.src = "../img/SunIcon.png"
-        // logo.src = "../img/logoWhite.png"
     } else {
         darktoggle.innerHTML = "Dark mode"
 
-        // icon.src = "../img/MoonIcon.png"
-        // logo.src = "../img/MainIcon.png"
     }
 }
